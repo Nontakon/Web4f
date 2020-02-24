a745f38a54c5c9647c59af91e413db7ce9d1037c
Nontakon <59010679@kmitl.ac.th>
2020-02-24 16:40:46 +0700

    hook datapart form backend

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -1,14 +1,24 @@
-import React, { useContext } from "react";
+import React, { useContext,useState, useEffect } from "react";
 import { CounterContext} from "../store/storeprovider"
 import {useHistory } from "react-router";
 import axios from "axios";
+import { Grid } from '@material-ui/core';
 
-
+interface PartInfo {
+  KKS: string
+  KKS1: string
+  NameEquip: string
+  KKS4: string
+  DateStart: Date
+  DateExpired: Date
+  Lift_time: number
+  CountStock: number
+}
 const Home:React.FC = () => {
     const { push } = useHistory () 
     const {KKS1, userName} = useContext(CounterContext)
     const logout = async() =>{
-        let info = await axios({
+        await axios({
             method: "get",
             responseType: "json",
             url: "http://localhost:5000/employee/clear_cookie",
@@ -21,6 +31,23 @@ const Home:React.FC = () => {
           });
         push('/LoginFrom')
     }
+    const [partInfo, setPartInfo] = useState([] as PartInfo[])
+    useEffect(()=>{
+      const fetching = async()=>{
+        try{
+          let { data } = await axios.get(`http://localhost:5000/equip_table/${KKS1}`)
+          // console.log(data)
+          setPartInfo(data)
+          
+        }catch(e){
+          console.log(e)
+        }
+    }
+    fetching()
+    },[KKS1])
+    const consol=()=>{
+      console.log(partInfo)
+    }
     return (
         <div>
             hi {userName}
@@ -28,6 +55,23 @@ const Home:React.FC = () => {
             <button onClick ={logout}>
                 log out
             </button>
+            <div>
+              {/* <Grid 
+                  container
+                  direction="row"
+                  justify="center"
+                  alignItems="center"
+                  spacing={1}
+                  >
+              <Grid container item xs={12} spacing={3}>
+
+              </Grid>
+            </Grid> */}
+            <button onClick ={consol}>
+                show console
+            </button>
+            </div>
+            
         </div>
     )
 }
