92b2f9d9ddbb8a5c6b1fe37d6dc6daecb85c7c86
Nontakon <59010679@kmitl.ac.th>
2020-02-26 13:52:01 +0700

    grid create

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -2,7 +2,8 @@ import React, { useContext,useState, useEffect } from "react";
 import { CounterContext} from "../store/storeprovider"
 import {useHistory } from "react-router";
 import axios from "axios";
-import { Grid } from '@material-ui/core';
+import styled, { css } from "styled-components";
+// import { Grid } from '@material-ui/core';
 
 interface PartInfo {
   KKS: string
@@ -11,7 +12,7 @@ interface PartInfo {
   KKS4: string
   DateStart: Date
   DateExpired: Date
-  Lift_time: number
+  Life_time: number
   CountStock: number
 }
 const Home:React.FC = () => {
@@ -48,32 +49,93 @@ const Home:React.FC = () => {
     const consol=()=>{
       console.log(partInfo)
     }
+    const editdata=()=>{
+      console.log(partInfo[0])
+    }
     return (
         <div>
-            hi {userName}
-            factory : {KKS1}
-            <button onClick ={logout}>
+          <div><button onClick ={logout}>
                 log out
             </button>
-            <div>
-              {/* <Grid 
-                  container
-                  direction="row"
-                  justify="center"
-                  alignItems="center"
-                  spacing={1}
-                  >
-              <Grid container item xs={12} spacing={3}>
-
-              </Grid>
-            </Grid> */}
+          </div>
+          <div>
+               factory : {KKS1}
+          </div>
+          <div>
+              hi {userName}
+          </div>
+          <div>
             <button onClick ={consol}>
                 show console
             </button>
-            </div>
             
+          </div> 
+          <div>
+            <Grid>
+              <Item>NameEquip</Item>
+              <Item>KKS</Item>
+              <Item>KKS1</Item>
+              <Item>KKS4</Item>
+              <Item>DateStart</Item>
+              <Item>DateExpired</Item>
+              <Item>Life_time</Item>
+              <Item>CountStock</Item>
+              {/* <Item>edit</Item> */}
+              {
+                partInfo.map(({NameEquip,KKS,KKS1,KKS4,DateStart,DateExpired,Life_time,CountStock})=><>
+                <p>{NameEquip}</p>
+                <p>{KKS}</p>
+                <p>{KKS1}</p>
+                <p>{KKS4}</p>
+                <p>{DateStart}</p>
+                <p>{DateExpired}</p>
+                <p>{Life_time}</p>
+                <p>{CountStock}</p>
+                </>)
+              }
+              {/* <Item>{partInfo[0].NameEquip}</Item> */}
+              {/* <Item>{partInfo[0].KKS}</Item>
+              <Item>{partInfo[0].KKS1}</Item>
+              <Item>{partInfo[0].KKS4}</Item>
+              <Item>{partInfo[0].DateStart}</Item>
+              <Item>{partInfo[0].DateExpired}</Item>
+              <Item>{partInfo[0].Life_time}</Item>
+              <Item>{partInfo[0].CountStock}</Item> */}
+               {/* <Item2><button onClick ={editdata}>edit</button></Item2> */}
+            </Grid>
+          </div>
         </div>
     )
 }
 
 export default Home
+
+export const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(8, 1fr);
+  grid-template-rows: repeat(10, 1fr);
+  grid-gap: 5px;
+  border-style: solid;
+  
+`;
+export const Item = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: .5rem;
+  border-style: dotted;
+  
+  ${css`
+      font-size: 18px;
+      font-weight: bold;
+    `}
+`;
+export const Item2 = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: .5rem;
+  border-style: dotted;
+  ${css`
+      font-size: 18px;
+      font-weight: bold;
+    `}
+`;
