c29cfe55d5f3a562a3757cc2a36bc86cb27ffeb2
DomeNTPS <tanndome_555@hotmail.com>
2020-03-09 21:05:47 +0700

    add export button

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -8,6 +8,12 @@ import MaterialTable, { Column } from 'material-table';
 import Button from "@material-ui/core/Button";
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import Cookies from "js-cookie";
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import TextField from '@material-ui/core/TextField';
 // import { Grid } from '@material-ui/core';
 
 interface PartInfo {
@@ -83,6 +89,17 @@ const Home:React.FC = () => {
     }
     fetching()
     },[])
+
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+    setOpen(true);
+    };
+
+    const handleClose = () => {
+    setOpen(false);
+    };
+
     return (
       <div>
         <Headnav>
@@ -138,6 +155,27 @@ const Home:React.FC = () => {
                 })
             }}
           />
+          <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+            Export
+      </Button>
+          <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+            <DialogTitle id="form-dialog-title">Withdraw Log</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                To subscribe to this website, please enter your email address here. We will send updates
+                occasionally.
+              </DialogContentText>
+              
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose} color="primary">
+                Cancel
+                </Button>
+                <Button onClick={handleClose} color="primary">
+                Export
+                </Button>
+            </DialogActions>
+          </Dialog>
         </Viewtable>
       </div>
     );
