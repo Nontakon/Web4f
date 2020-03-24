37b5b20d72fce973b1321a391fb1ad6d196dba4e
Jakkapat Boonroj <max.jakkapat@gmail.com>
2020-03-24 22:40:27 +0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,27 @@
-import React from 'react';
-import './App.css';
-import LoginForm from './component/LoginFrom';
-import Home from './component/Home'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-import { Storeprovider } from './store/storeprovider';
+import React from "react";
+import "./App.css";
+import LoginForm from "./component/LoginFrom";
+import Home from "./component/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Storeprovider } from "./store/storeprovider";
 
 function App() {
+  /* Hello */
   return (
     <Storeprovider>
       <Router>
-      <div>
-        <Switch>
-          <Route path="/Home">
-            <Home />
-          </Route>
-          <Route path="/">
-            <LoginForm />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+        <div>
+          <Switch>
+            <Route path="/Home">
+              <Home />
+            </Route>
+            <Route path="/">
+              <LoginForm />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </Storeprovider>
-   
   );
 }
 
