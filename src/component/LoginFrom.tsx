c6dbb5f772a1ef14d2500e9e39dbc9c34e8eb08e
Jakkapat Boonroj <max.jakkapat@gmail.com>
2020-03-25 21:05:09 +0700

    Revert "fix bug localhost"
    This reverts commit c8402d64a31d8d5f764ca7b57480266dc916d697.

--- src/component/LoginFrom.tsx
+++ src/component/LoginFrom.tsx
@@ -1,64 +1,61 @@
-import React, { useContext,useState, useEffect } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import axios from "axios";
-import {
-  useHistory 
-} from "react-router";
-import { CounterContext} from "../store/storeprovider";
-import styles from '../css_style/inputStyle.module.css';
-import { AccessAlarm, ThreeDRotation } from '@material-ui/icons';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import { makeStyles } from '@material-ui/core/styles';
-import LockIcon from '@material-ui/icons/Lock';
-import logo from "../css_style/Logo_kmitl.png"
+import { useHistory } from "react-router";
+import { CounterContext } from "../store/storeprovider";
+import styles from "../css_style/inputStyle.module.css";
+import { AccessAlarm, ThreeDRotation } from "@material-ui/icons";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import { makeStyles } from "@material-ui/core/styles";
+import LockIcon from "@material-ui/icons/Lock";
+import logo from "../css_style/Logo_kmitl.png";
 import styled from "styled-components";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import Cookies from "js-cookie";
 
-
 export default function LoginFrom() {
-
-  const { push } = useHistory () 
+  const { push } = useHistory();
   const { register, handleSubmit, errors } = useForm({
     defaultValues: {
       id: "",
       password: ""
     }
   });
-  const {KKS1,userName,addKKS1, adduserName } = useContext(CounterContext)
-  const chacktoken = async() => {
-    if(Cookies.get(`access_token`)!==undefined){
-      push('/Home')
+  const { KKS1, userName, addKKS1, adduserName } = useContext(CounterContext);
+  const chacktoken = async () => {
+    if (Cookies.get(`access_token`) !== undefined) {
+      push("/Home");
     }
-  }
-  useEffect(()=>{
-      const fetching = async()=>{
-        try{
-          chacktoken()
-        }catch(e){
-          console.log(e)
-        }
-    }
-    fetching()
-    },[])
+  };
+  useEffect(() => {
+    const fetching = async () => {
+      try {
+        chacktoken();
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetching();
+  }, []);
   const onSubmit = async (data: any) => {
     try {
-      console.log(data)
+      console.log(data);
       const info = await axios.post(
-        `:5000/employee/web`,
+        `${process.env.REACT_APP_SERVER_URI}employee/web`,
         {
           ID: `${data.id}`,
           Pass: `${data.password}`
         }
       );
       console.log(info);
-      addKKS1(info.data.KKS1)
-      adduserName(info.data.user)
-      await Cookies.set(`access_token`, `${info.data.token}`,{ expires: (new Date(new Date().getTime() + 16*60*60*1000))})
-      push('/Home')
-
+      addKKS1(info.data.KKS1);
+      adduserName(info.data.user);
+      await Cookies.set(`access_token`, `${info.data.token}`, {
+        expires: new Date(new Date().getTime() + 16 * 60 * 60 * 1000)
+      });
+      push("/Home");
     } catch (e) {
       console.log(e);
     }
@@ -73,9 +70,7 @@ export default function LoginFrom() {
           <StyledTextField
             id="standard-basic"
             label="ID"
-            inputRef={
-              register({ required: true, pattern: /([0-9]){13}/i })
-            }
+            inputRef={register({ required: true, pattern: /([0-9]){13}/i })}
             type="text"
             name="id"
             InputProps={{
@@ -92,7 +87,7 @@ export default function LoginFrom() {
             type="password"
             autoComplete="current-password"
             name="password"
-            inputRef={ register({ required: true, min: 1 })}
+            inputRef={register({ required: true, min: 1 })}
             InputProps={{
               startAdornment: (
                 <InputAdornment position="start">
@@ -116,7 +111,7 @@ const Container = styled.div`
 `;
 const Background = styled.div`
   display: flex;
-  min-height:90vh;
+  min-height: 90vh;
 `;
 const RootAccountCircleIcon = styled(AccountCircleIcon)`
   &&& {
