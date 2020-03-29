407408e58ead21b6c94328941733a2abfaf5a4ed
Jakkapat Boonroj <max.jakkapat@gmail.com>
2020-03-29 18:31:58 +0700

    auto gen qrcode Co-authored-by: DomeNTPS <tanndome_555@hotmail.com>

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -21,7 +21,7 @@ import Tab from "@material-ui/core/Tab";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
 import DialogExport from "./Home/components/DialogExport";
-// import QRCode from 'qrcode.react' error
+import QRCode from "qrcode.react";
 // import { Grid } from '@material-ui/core';
 
 interface TabPanelProps {
@@ -68,11 +68,11 @@ interface TableState {
   columns: Array<Column<PartInfo>>;
   data: PartInfo[];
 }
-interface CountAdd{
-  CountADD : number;
+interface CountAdd {
+  CountADD: number;
 }
-interface CountReturn{
-  CountReturn : number|any;
+interface CountReturn {
+  CountReturn: number | any;
 }
 const Home: React.FC = () => {
   const { push } = useHistory();
@@ -101,8 +101,10 @@ const Home: React.FC = () => {
       );
       // console.log(infodata.data)
       setState(prev => ({ ...prev, data: infodata.data }));
-      let IDEmp = await axios.get(`${process.env.REACT_APP_SERVER_URI}returnwithdraw/allID/${infouser.data[0].KKS1_factory}`)
-      setID(IDEmp.data)
+      let IDEmp = await axios.get(
+        `${process.env.REACT_APP_SERVER_URI}returnwithdraw/allID/${infouser.data[0].KKS1_factory}`
+      );
+      setID(IDEmp.data);
     } else {
       push("/LoginFrom");
     }
@@ -128,7 +130,7 @@ const Home: React.FC = () => {
 
   const [openadd, setOpenadd] = React.useState(false);
 
-  useEffect(() =>{
+  useEffect(() => {
     chacktoken();
   }, []);
 
@@ -138,112 +140,141 @@ const Home: React.FC = () => {
   const handleCloseAdd = () => {
     setOpenadd(false);
   };
-  var now = dayjs().format('YYYY-MM-DD HH:mm:ss')
-  const handleAdd = async() => {
-    let CountNow = await axios.post(`${process.env.REACT_APP_SERVER_URI}updateinventory/selectcount`,{
-      NameEquip: `${equipName}`,
-      KKS1 : `${userinfo.KKS1_factory}`
-    })
-    await axios.post(`${process.env.REACT_APP_SERVER_URI}updateinventory/`,{
-      CountStock :`${CountNow.data[0].CountStock + count.CountADD}`,
-      KKS1 : `${userinfo.KKS1_factory}`,
-      KKS4 : `${CountNow.data[0].KKS4}`
-    })
-    await axios.post(`${process.env.REACT_APP_SERVER_URI}insertlog/`,{
+  var now = dayjs().format("YYYY-MM-DD HH:mm:ss");
+  const handleAdd = async () => {
+    let CountNow = await axios.post(
+      `${process.env.REACT_APP_SERVER_URI}updateinventory/selectcount`,
+      {
+        NameEquip: `${equipName}`,
+        KKS1: `${userinfo.KKS1_factory}`
+      }
+    );
+    await axios.post(`${process.env.REACT_APP_SERVER_URI}updateinventory/`, {
+      CountStock: `${CountNow.data[0].CountStock + count.CountADD}`,
+      KKS1: `${userinfo.KKS1_factory}`,
+      KKS4: `${CountNow.data[0].KKS4}`
+    });
+    await axios.post(`${process.env.REACT_APP_SERVER_URI}insertlog/`, {
       token: `${token}`,
-      Process: 'AddEquipment',
-      KKS1 : `${userinfo.KKS1_factory}`,
-      KKS4 : `${CountNow.data[0].KKS4}`,
+      Process: "AddEquipment",
+      KKS1: `${userinfo.KKS1_factory}`,
+      KKS4: `${CountNow.data[0].KKS4}`,
       Countlog: `${count.CountADD}`,
-      Datelog : `${now}`
-    })
+      Datelog: `${now}`
+    });
     setOpenadd(false);
-  }
+  };
 
-  const handleId = async() => {
-    let CountNow = await axios.post(`${process.env.REACT_APP_SERVER_URI}updateinventory/selectcount`,{ 
-      NameEquip: `${nameEquipSelect}`,
-      KKS1 : `${userinfo.KKS1_factory}`
-    })
-    await axios.post(`${process.env.REACT_APP_SERVER_URI}updateinventory/`,{
-      CountStock :CountNow.data[0].CountStock + countwithdrawSelect,
-      KKS1 : `${userinfo.KKS1_factory}`,
-      KKS4 : `${CountNow.data[0].KKS4}`,
+  const handleId = async () => {
+    let CountNow = await axios.post(
+      `${process.env.REACT_APP_SERVER_URI}updateinventory/selectcount`,
+      {
+        NameEquip: `${nameEquipSelect}`,
+        KKS1: `${userinfo.KKS1_factory}`
+      }
+    );
+    await axios.post(`${process.env.REACT_APP_SERVER_URI}updateinventory/`, {
+      CountStock: CountNow.data[0].CountStock + countwithdrawSelect,
+      KKS1: `${userinfo.KKS1_factory}`,
+      KKS4: `${CountNow.data[0].KKS4}`,
       IDEmp: `${iduseSelect}`
-    })
-    await axios.post(`${process.env.REACT_APP_SERVER_URI}insertwithdraw/web`,{
-      Countwithdraw :broke,
-      KKS1 : `${userinfo.KKS1_factory}`,
-      KKS4 : `${CountNow.data[0].KKS4}`,
+    });
+    await axios.post(`${process.env.REACT_APP_SERVER_URI}insertwithdraw/web`, {
+      Countwithdraw: broke,
+      KKS1: `${userinfo.KKS1_factory}`,
+      KKS4: `${CountNow.data[0].KKS4}`,
       IDEmp: `${iduseSelect}`
-    })
-    await axios.post(`${process.env.REACT_APP_SERVER_URI}insertbrokeequip/`,{
-      CountBroke :broke,
-      KKS1 : `${userinfo.KKS1_factory}`,
-      KKS4 : `${CountNow.data[0].KKS4}`
-    })
-    await axios.post(`${process.env.REACT_APP_SERVER_URI}insertlog/return`,{
+    });
+    await axios.post(`${process.env.REACT_APP_SERVER_URI}insertbrokeequip/`, {
+      CountBroke: broke,
+      KKS1: `${userinfo.KKS1_factory}`,
+      KKS4: `${CountNow.data[0].KKS4}`
+    });
+    await axios.post(`${process.env.REACT_APP_SERVER_URI}insertlog/return`, {
       IDEmp: `${iduseSelect}`,
-      Process: 'ReturnEquipment',
-      KKS1 : `${userinfo.KKS1_factory}`,
-      KKS4 : `${CountNow.data[0].KKS4}`,
+      Process: "ReturnEquipment",
+      KKS1: `${userinfo.KKS1_factory}`,
+      KKS4: `${CountNow.data[0].KKS4}`,
       Countlog: countwithdrawSelect,
-      Datelog : `${now}`
-    })
-    await axios.post(`${process.env.REACT_APP_SERVER_URI}insertlog/return`,{
+      Datelog: `${now}`
+    });
+    await axios.post(`${process.env.REACT_APP_SERVER_URI}insertlog/return`, {
       IDEmp: `${iduseSelect}`,
-      Process: 'EquipmentBroke',
-      KKS1 : `${userinfo.KKS1_factory}`,
-      KKS4 : `${CountNow.data[0].KKS4}`,
+      Process: "EquipmentBroke",
+      KKS1: `${userinfo.KKS1_factory}`,
+      KKS4: `${CountNow.data[0].KKS4}`,
       Countlog: broke,
-      Datelog : `${now}`
-    })
+      Datelog: `${now}`
+    });
     setOpenadd(false);
-    console.log(iduseSelect)
-    console.log(nameEquipSelect)
-    console.log(countwithdrawSelect)
-    console.log(broke)
-  }
+    console.log(iduseSelect);
+    console.log(nameEquipSelect);
+    console.log(countwithdrawSelect);
+    console.log(broke);
+  };
 
   const [valuetab, setValuetab] = React.useState("one"); //problem
 
   const handleChange = (event: React.ChangeEvent<{}>, newValue: string) => {
     setValuetab(newValue);
   };
-  const [count,setCount] = React.useState<CountAdd>({CountADD:0});
+  const [count, setCount] = React.useState<CountAdd>({ CountADD: 0 });
 
   const [equipName, setEquipName] = React.useState<string[]>([]);
 
   const handleChangeEquip = (event: React.ChangeEvent<{ value: unknown }>) => {
     setEquipName(event.target.value as string[]);
   };
-  const handleChangeQuantity = (event: React.ChangeEvent<{ value: unknown }>) =>{
-    setCount({CountADD : parseInt(event.target.value as string ,10)});
-  }
+  const handleChangeQuantity = (
+    event: React.ChangeEvent<{ value: unknown }>
+  ) => {
+    setCount({ CountADD: parseInt(event.target.value as string, 10) });
+  };
 
-  const [idemp, setID] = React.useState<string[]|any>([]);
+  const [idemp, setID] = React.useState<string[] | any>([]);
   const [iduseSelect, setIDselect] = React.useState<string[]>([]);
-  const [nameEquipforReturn, setnameEquipforReturn] = React.useState<string[]|any>([]);
+  const [nameEquipforReturn, setnameEquipforReturn] = React.useState<
+    string[] | any
+  >([]);
   const [nameEquipSelect, setnameEquipSelect] = React.useState<string[]>([]);
-  const [countwithdrawforReturn, setcountwithdrawforReturn] = React.useState<CountReturn>({CountReturn:0});
-  const [countwithdrawSelect, setcountwithdrawSelect] = React.useState<number>();
+  const [countwithdrawforReturn, setcountwithdrawforReturn] = React.useState<
+    CountReturn
+  >({ CountReturn: 0 });
+  const [countwithdrawSelect, setcountwithdrawSelect] = React.useState<
+    number
+  >();
   const [broke, setBroke] = React.useState<number>(0);
 
-  const handleChangeID = async (event: React.ChangeEvent<{ value: unknown }>) => {
+  const handleChangeID = async (
+    event: React.ChangeEvent<{ value: unknown }>
+  ) => {
     setIDselect(event.target.value as string[]);
-    let nameEquip = await axios.get(`${process.env.REACT_APP_SERVER_URI}returnwithdraw/allName/${event.target.value}`) 
-    setnameEquipforReturn(nameEquip.data)
-  }
-  const handleChangeEquipforReturn = async (event: React.ChangeEvent<{ value: unknown }>) => {
+    let nameEquip = await axios.get(
+      `${process.env.REACT_APP_SERVER_URI}returnwithdraw/allName/${event.target.value}`
+    );
+    setnameEquipforReturn(nameEquip.data);
+  };
+  const handleChangeEquipforReturn = async (
+    event: React.ChangeEvent<{ value: unknown }>
+  ) => {
     setnameEquipSelect(event.target.value as string[]);
-    let CountWithdraw = await axios.post(`${process.env.REACT_APP_SERVER_URI}returnwithdraw/Count`,{IDEmp : `${iduseSelect}`,NameEquip : `${event.target.value}` }) 
-    setcountwithdrawforReturn({CountReturn : CountWithdraw.data.Count_withdraw})
-  }
-  const handleChangeCountwithdrawforReturn = async (event: React.ChangeEvent<{ value: unknown }>) => {
-    setcountwithdrawSelect(parseInt(event.target.value as string ,10));
-    setBroke(countwithdrawforReturn.CountReturn- parseInt(event.target.value as string ,10))
-  }
-
+    let CountWithdraw = await axios.post(
+      `${process.env.REACT_APP_SERVER_URI}returnwithdraw/Count`,
+      { IDEmp: `${iduseSelect}`, NameEquip: `${event.target.value}` }
+    );
+    setcountwithdrawforReturn({
+      CountReturn: CountWithdraw.data.Count_withdraw
+    });
+  };
+  const handleChangeCountwithdrawforReturn = async (
+    event: React.ChangeEvent<{ value: unknown }>
+  ) => {
+    setcountwithdrawSelect(parseInt(event.target.value as string, 10));
+    setBroke(
+      countwithdrawforReturn.CountReturn -
+        parseInt(event.target.value as string, 10)
+    );
+  };
 
   return (
     <div>
@@ -261,30 +292,32 @@ const Home: React.FC = () => {
         <Infoview>
           {userinfo.NameEmp} &nbsp;{userinfo.LastNameEmp} &nbsp; factory :{" "}
           {remove_last_character(userinfo.KKS1_factory)}{" "}
-          <AddBut variant="outlined" color="primary" onClick={handleClickOpenAdd}>
+          <AddBut
+            variant="outlined"
+            color="primary"
+            onClick={handleClickOpenAdd}
+          >
             Manage Item
-        </AddBut>
+          </AddBut>
         </Infoview>
         <MaterialTable
           title="EquipmentData"
           style={{ borderRadius: "15" }}
           columns={state.columns}
-          data={state.data.map(({ DateStart, DateExpired,QRCode=false,...rest }) => {
-            console.log({QRCode})
+          data={state.data.map(({ DateStart, DateExpired, KKS, ...rest }) => {
             return {
               ...rest,
+              KKS,
               DateStart: dayjs(DateStart).format("DD/MM/YYYY"),
               DateExpired: dayjs(DateExpired).format("DD/MM/YYYY"),
-              QRCode : QRCode&&<img style={{width:75}} src = {QRCode}/>
+              QRCode: QRCode && <QRCode value={KKS} size={90} />
               // QRCode : <a href = {QRCode} target = "_blank">{QRCode}</a>,
-            }
-            
-          })
-        }
+            };
+          })}
         />
 
-        <DialogExport plantNumber={userinfo.KKS1_factory}/>
-        
+        <DialogExport plantNumber={userinfo.KKS1_factory} />
+
         <Dialog
           fullWidth={true}
           maxWidth={"lg"}
@@ -316,12 +349,18 @@ const Home: React.FC = () => {
                     input={<Input id="demo-dialog-native" />}
                   >
                     <option aria-label="None" value="" />
-                    <option value={'Valve'}>Valve</option>
-                    <option value={'Flow rate meter'}>Flow rate meter</option>
-                    <option value={'Presure meter'}>Presure meter</option>
+                    <option value={"Valve"}>Valve</option>
+                    <option value={"Flow rate meter"}>Flow rate meter</option>
+                    <option value={"Presure meter"}>Presure meter</option>
                   </Select>
                 </FormControl>
-                <TextField id="standard-basic" label="Quantity" className={classes.formControl} onChange={handleChangeQuantity} type="number" />
+                <TextField
+                  id="standard-basic"
+                  label="Quantity"
+                  className={classes.formControl}
+                  onChange={handleChangeQuantity}
+                  type="number"
+                />
               </form>
               <Button onClick={handleAdd} color="primary">
                 Add
@@ -332,26 +371,23 @@ const Home: React.FC = () => {
             </TabPanel>
             <TabPanel value={valuetab} index="two">
               <form>
-                {/* <TextField id="standard-basic" label="ID" className={classes.formControl} onChange={handleChangeID} /> */}
+                {/* <TextField id="standard-basic" label="ID" cnpm install @types/qrcode.reactlassName={classes.formControl} onChange={handleChangeID} /> */}
                 <FormControl className={classes.formControl}>
-                  <InputLabel htmlFor="demo-dialog-native">
-                    ID
-                  </InputLabel>
+                  <InputLabel htmlFor="demo-dialog-native">ID</InputLabel>
                   <Select
                     native
                     value={iduseSelect}
                     onChange={handleChangeID}
                     input={<Input id="demo-dialog-native" />}
                   >
-                   <option aria-label="None" value="" />
-                   {[...new Array(idemp.length)].map((_, i) => {
-                                    return (
-                                        <option key={i}
-                                            value={idemp[i].IDEmp}>
-                                            {idemp[i].IDEmp}
-                                        </option>
-                                    );
-                                })}
+                    <option aria-label="None" value="" />
+                    {[...new Array(idemp.length)].map((_, i) => {
+                      return (
+                        <option key={i} value={idemp[i].IDEmp}>
+                          {idemp[i].IDEmp}
+                        </option>
+                      );
+                    })}
                   </Select>
                 </FormControl>
                 <FormControl className={classes.formControl}>
@@ -366,19 +402,16 @@ const Home: React.FC = () => {
                   >
                     <option aria-label="None" value="" />
                     {[...new Array(nameEquipforReturn.length)].map((_, i) => {
-                                    return (
-                                        <option
-                                            value={nameEquipforReturn[i].NameEquip}>
-                                            {nameEquipforReturn[i].NameEquip}
-                                        </option>
-                                    );
-                                })}
+                      return (
+                        <option value={nameEquipforReturn[i].NameEquip}>
+                          {nameEquipforReturn[i].NameEquip}
+                        </option>
+                      );
+                    })}
                   </Select>
                 </FormControl>
                 <FormControl className={classes.formControl}>
-                  <InputLabel htmlFor="demo-dialog-native">
-                    Quantity
-                  </InputLabel>
+                  <InputLabel htmlFor="demo-dialog-native">Quantity</InputLabel>
                   <Select
                     native
                     value={countwithdrawSelect}
@@ -386,15 +419,12 @@ const Home: React.FC = () => {
                     input={<Input id="demo-dialog-native" />}
                   >
                     <option aria-label="None" value="" />
-                     <option value={0}>0</option>
-                    {[...new Array(countwithdrawforReturn.CountReturn)].map((_, i) => {
-                                      return (
-                                        <option
-                                          value={++i}>
-                                          {i}
-                                        </option>
-                                      );
-                                })}
+                    <option value={0}>0</option>
+                    {[...new Array(countwithdrawforReturn.CountReturn)].map(
+                      (_, i) => {
+                        return <option value={++i}>{i}</option>;
+                      }
+                    )}
                   </Select>
                 </FormControl>
                 {/* <FormControl className={classes.formControl}>
@@ -410,9 +440,11 @@ const Home: React.FC = () => {
                   </Select>
                 </FormControl> */}
                 <FormControl disabled className={classes.formControl}>
-                  <InputLabel htmlFor="component-disabled">Broke Item</InputLabel>
-                  <Input id="component-disabled" value={broke}/>
-                </FormControl> 
+                  <InputLabel htmlFor="component-disabled">
+                    Broke Item
+                  </InputLabel>
+                  <Input id="component-disabled" value={broke} />
+                </FormControl>
               </form>
               <Button onClick={handleId} color="primary">
                 Add
@@ -463,7 +495,7 @@ const Viewtable = styled.div`
   border-radius: 3px;
 `;
 const AddBut = styled(Button)`
-  &&&{
+  &&& {
     position: absolute;
     right: 30px;
     margin-top: 3px;
